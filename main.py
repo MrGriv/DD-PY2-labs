c5b799f884d60f95250e2d5328fb520a1b1b0ecc
MrGriv <115357430+MrGriv@users.noreply.github.com>
2022-12-14 23:11:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,33 +2,29 @@ import doctest
 from typing import Union
 
 class Bicycle:
-
     def __init__(self, speed: int, brakes: str, wheel_size: Union[int, float]):
         """
         Создание и подготовка к работе объекта велосипед
-
         :param speed: количество скоростей заднего переключателя
         :param brakes: система тормозов
         :param wheel_size: размер колес
-
         Пример:
-        >>> bike26 = Bicycle(12, 'Disk', 27.5)  # инициализация экземпляра класса
+        >>> bike26 = Bicycle(12, 'disk', 27.5)  # инициализация экземпляра класса
         """
-        self.speed = 10
         self.init_bicycle_sp(speed)
-        self.brakes = 'Disk'
+        self.speed = speed
         self.init_brakes_sis(brakes)
-        self.wheel_size = 29
+        self.brakes = brakes
         self.init_wheel_size(wheel_size)
+        self.wheel_size = wheel_size
 
     def init_bicycle_sp(self, speed):
         """
         инициализация скоростей велосипеда
-
         :param speed: скорости велосипеда
         :return: скорости велосипеда
         """
-        if not isinstance(speed, int):
+        if not isinstance(speed, (int, float)):
             raise TypeError
         if speed < 0:
             raise ValueError
@@ -36,10 +32,9 @@ class Bicycle:
 
     def init_brakes_sis(self, brakes):
         """
-        инициализация торсозной системы велосипеда
-
-        :param brakes: торсозная система велосипеда
-        :return: торсозная система велосипеда
+        Инициализация тормозной системы велосипеда
+        :param brakes: тормозная система велосипеда
+        :return: тормозная система велосипеда
         """
         if not isinstance(brakes, str):
             raise TypeError
@@ -47,8 +42,7 @@ class Bicycle:
 
     def init_wheel_size(self, wheel_size):
         """
-        инициализация размера колес велосипеда
-
+        Инициализация размера колес велосипеда
         :param wheel_size: размер колес велосипеда
         :return: размер колес велосипеда
         """
@@ -61,22 +55,17 @@ class Bicycle:
     def change_chain(self) -> None:
         """
         Замена цепи
-
-        :return: Новая цепь
-
         Пример
-        >>> bike26 = Bicycle(12, 'Disk', 27.5)
+        >>> bike26 = Bicycle(12, 'disk', 27.5)
         >>> bike26.change_chain()
         """
         ...
     def check_distance(self) -> int:
         """
         Проверка пройденного расстояние
-
         :return: расстояние
-
         Пример
-        >>> bike26 = Bicycle(12, 'Disk', 27.5)
+        >>> bike26 = Bicycle(12, 'disk', 27.5)
         >>> bike26.check_distance()
         """
         ...
@@ -86,29 +75,26 @@ class SelfTappingScrew:
     def __init__(self, size: int, type_of_screw: str, color: str):
         """
         Создание и подготовка к работе объекта саморез
-
         :param size: Размер самореза
         :param type_of_screw: Тип самореза
         :param color: Цвет самореза
-
         Пример
-        >>> metal_screw = SelfTappingScrew(25, 'For metal', 'White')
+        >>> metal_screw = SelfTappingScrew(25, 'for metal', 'white')
         """
-        self.size = 45
         self.init_size(size)
-        self.type_of_screw = 'For wood'
+        self.size = size
         self.init_type(type_of_screw)
-        self.color = 'Black'
+        self.type_of_screw = type_of_screw
         self.init_color(color)
+        self.color = color
 
     def init_size(self, size):
         """
         Инициализация размера самореза
-
         :param size: размер самореза
         :return: размер самореза
         """
-        if not isinstance(size, int):
+        if not isinstance(size, (int, float)):
             raise TypeError
         if size < 0:
             raise ValueError
@@ -117,7 +103,6 @@ class SelfTappingScrew:
     def init_type(self, type_of_screw):
         """
         Инициализация типа самореза
-
         :param type_of_screw: тип самореза
         :return: тип самореза
         """
@@ -128,7 +113,6 @@ class SelfTappingScrew:
     def init_color(self, color):
         """
         Инициализация цвета самореза
-
         :param color: цвет самореза
         :return: цвет самореза
         """
@@ -139,11 +123,8 @@ class SelfTappingScrew:
     def use_self_tapping_screw(self) -> None:
         """
         Использование самореза
-
-        :return: Закрученный саморез
-
         Пример
-        >>> metal_screw = SelfTappingScrew(25, 'For metal', 'White')
+        >>> metal_screw = SelfTappingScrew(25, 'for metal', 'white')
         >>> metal_screw.use_self_tapping_screw()
         """
         ...
@@ -151,36 +132,30 @@ class SelfTappingScrew:
     def count_screws(self) -> int:
         """
         Пересчет саморезов
-
         :return: количество саморезов
-
         Пример
-        >>> metal_screw = SelfTappingScrew(25, 'For metal', 'White')
+        >>> metal_screw = SelfTappingScrew(25, 'for metal', 'white')
         >>> metal_screw.count_screws()
         """
         ...
 
 class Apple:
-
     def __init__(self, color: str, amount: int):
         """
         Создание и подготовка к работе объекта яблоко
-
         :param color: цвет яблока
         :param amount: количество яблок
-
         Пример
-        >>> apple = Apple('Red', 7)
+        >>> apple = Apple('red', 7)
         """
-        self.color = 'Green'
         self.init_color(color)
-        self.amount = 0
+        self.color = color
         self.init_amount(amount)
+        self.amount = amount
 
     def init_color(self, color):
         """
         Инициализация цвета яблок
-
         :param color: цвет яблок
         :return: цвет яблок
         """
@@ -191,11 +166,10 @@ class Apple:
     def init_amount(self, amount):
         """
         Инициализация количества яблок
-
         :param amount: количество яблок
         :return: количество яблок
         """
-        if not isinstance(amount, int):
+        if not isinstance(amount, (int, float)):
             raise TypeError
         if amount < 0:
             raise ValueError
@@ -204,11 +178,8 @@ class Apple:
     def eat_apple(self) -> None:
         """
         Съесть яблоко
-
-        :return: ничего
-
         Пример
-        >>> apple = Apple('Red', 7)
+        >>> apple = Apple('red', 7)
         >>> apple.eat_apple()
         """
         ...
@@ -216,12 +187,10 @@ class Apple:
     def add_apples(self, amount) -> int:
         """
         Добавить яблок
-
         :param amount: исходное количество яблок
         :return: новое количество яблок
-
         Пример
-        >>> apple = Apple('Red', 7)
+        >>> apple = Apple('red', 7)
         >>> apple.add_apples(7)
         """
         ...
