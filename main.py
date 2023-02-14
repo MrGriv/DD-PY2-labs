f1cd85035a7d228c39b196f3c60796e8ae88afb4
MrGriv <115357430+MrGriv@users.noreply.github.com>
2023-02-14 15:17:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,18 @@ class Book:
     def __str__(self):
         return f"Книга {self._name}. Автор {self._author}"
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
-
 
 class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
         super().__init__(name, author)
         self.pages = pages
 
-    def __str__(self):
-        return f"Книга {self._name}. Автор {self._author}. Страницы {self.pages}"
-
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.pages!r})"
 
     @property
     def pages(self) -> int:
-        return self.pages
+        return self._new_pages
 
     @pages.setter
     def pages(self, pages: int) -> None:
@@ -32,6 +26,7 @@ class PaperBook(Book):
             raise ValueError('Количество страниц меньше 0')
         if type(pages) != int:
             raise TypeError('Требуется целочисленное значение')
+        self._new_pages = pages
 
 
 class AudioBook(Book):
@@ -39,15 +34,12 @@ class AudioBook(Book):
         super().__init__(name, author)
         self.duration = duration
 
-    def __str__(self):
-        return f"Книга {self._name}. Автор {self._author}. Длительность {self.duration}"
-
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration!r})"
+        return f'{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration!r})'
 
     @property
     def duration(self) -> float:
-        return self.duration
+        return self._new_duration
 
     @duration.setter
     def duration(self, value) -> None:
@@ -55,7 +47,10 @@ class AudioBook(Book):
             raise ValueError('Длительность аудиокниги меньше 0')
         if type(value) != float:
             raise TypeError('Требуется число с плавающей запятой')
+        self._new_duration = value
 
 
-ebook = AudioBook('Name', 'Author', 1.0)
+ebook = AudioBook(name='Name', author='Author', duration=1.0)
+paper = PaperBook(name='P', author='IA', pages=123)
 print(ebook)
+print(paper)
